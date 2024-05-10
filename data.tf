75336a12564dce11d2ab71b0b39d8c2ddedd1eff
vardy.ng <wadee@wadeenghoiwa.com>
2024-05-10 15:49:52 -0400

    format tf code

--- data.tf
+++ data.tf
@@ -4,7 +4,7 @@ locals {
   all_regions = jsondecode((file("${path.module}/data/region.json")))["region"]
 
   # specical region
-  china_region = jsondecode((file("${path.module}/data/china-region.json")))["region"]
+  china_region     = jsondecode((file("${path.module}/data/china-region.json")))["region"]
   gov_cloud_region = jsondecode((file("${path.module}/data/gov-cloud-region.json")))["region"]
 
   input_region = replace(var.region, "-", "") # replace "-" in the region string
@@ -13,9 +13,9 @@ locals {
   is_valid_char_code = contains([for region in local.all_regions : region.char_code], local.input_region) ? var.region : file("[Error] ${var.region} is not valid")
 
   # is china region
-  is_china_region = contains(local.china_region, local.input_region)
+  is_china_region     = contains(local.china_region, local.input_region)
   is_gov_cloud_region = contains(local.gov_cloud_region, local.input_region)
-  is_global_region = local.is_china_region == false && local.is_gov_cloud_region == false
+  is_global_region    = local.is_china_region == false && local.is_gov_cloud_region == false
 }
 
 
