a5835939ef7d76b69c160c416417ca479244170f
vardy.ng <wadee@wadeenghoiwa.com>
2024-05-10 14:13:08 -0400

    add module output

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "is_global_region" {
+  value = local.is_global_region
+}
+
+output "is_gov_cloud_region" {
+  value = local.is_gov_cloud_region
+}
+
+
+output "is_china_region" {
+  value = local.is_china_region
+}
